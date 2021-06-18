f86b5b1a55c8c4a92eefe3f649f928f9af97ae2b
Mathis Houlet <houletmathis@gmail.com>
2021-06-18 14:36:51 +0200

    init profil page

--- omh-ionic/src/app/app-routing.module.ts
+++ omh-ionic/src/app/app-routing.module.ts
@@ -26,6 +26,10 @@ const routes: Routes = [
     path: 'signup',
     loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
   },
+  {
+    path: 'profil',
+    loadChildren: () => import('./profil/profil.module').then( m => m.ProfilPageModule)
+  },
 ];
 
 @NgModule({
